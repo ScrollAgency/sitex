ff704f54e82e35775b31e527edca45975595be9c
gael <prigentgael@gmail.com>
2025-02-24 16:49:11 +0100

    ✨ Update with new plasmic-library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 
 import "@uppy/core/dist/style.min.css";
 import "@uppy/dashboard/dist/style.min.css";
-import '../styles/globals.css'; // Adjust the path if necessary
+import '@/styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

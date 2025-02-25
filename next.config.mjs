f8ecb0291573c61e0b88848c240e760c30e29b01
gael <prigentgael@gmail.com>
2025-02-25 15:46:44 +0100

    Fix upadate module.css

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push(
+      {
+        test: /\.d\.ts$/,
+        use: "null-loader", // Ignore les fichiers TypeScript de définition de types
+      },
+      {
+        test: /\.sh$/,
+        use: "null-loader", // Ignore les scripts shell
+      }
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
